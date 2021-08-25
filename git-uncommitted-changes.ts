8045c4babb2f9ce9964f12d74f14958ad77b4332
John Lyon-Smith <jolyonsm@amazon.com>
2021-08-24 17:10:41 -0700

    Update Rust release to run coverage

--- git-uncommitted-changes.ts
+++ git-uncommitted-changes.ts
@@ -1,15 +1,7 @@
 #!/usr/bin/env -S deno run --allow-read --allow-run
 console.info("Checking for uncommitted changes...")
 
-if (
-  !(
-    await Deno.run({
-      cmd: ["git", "diff-index", "--quiet", "HEAD", "--"],
-      stderr: "null",
-      stdout: "null",
-    }).status()
-  ).success
-) {
+if (isUncommittedChanges()) {
   console.error(
     "There are uncomitted changes - commit or stash them and try again"
   )
@@ -17,3 +9,13 @@ if (
 }
 
 Deno.exit(0)
+
+export default async function isUncommittedChanges() {
+  return !(
+    await Deno.run({
+      cmd: ["git", "diff-index", "--quiet", "HEAD", "--"],
+      stderr: "null",
+      stdout: "null",
+    }).status()
+  ).success
+}
